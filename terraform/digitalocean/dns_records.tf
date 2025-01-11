8ff88984a5f14f3cb8691bffa929e4263ede08e1
Joey Grasso <me@joeygrasso.com>
2025-01-11 18:40:15 -0500

    Added glynbrook domain and dns records

--- terraform/digitalocean/dns_records.tf
+++ terraform/digitalocean/dns_records.tf
@@ -1,4 +1,44 @@
+##########
+# glynbrook.com
+##########
+resource "digitalocean_record" "www_glynbrook_com" {
+  domain = digitalocean_domain.glynbrook_com.id
+  type   = "A"
+  name   = "www"
+  value  = digitalocean_reserved_ip.web-nyc1-cpunation-com-reserved-ip.ip_address
+}
+
+resource "digitalocean_record" "glynbrook_com" {
+  domain = digitalocean_domain.glynbrook_com.id
+  type   = "A"
+  name   = "@"
+  value  = digitalocean_reserved_ip.web-nyc1-cpunation-com-reserved-ip.ip_address
+}
+
+resource "digitalocean_record" "ns1_glynbrook_com" {
+  domain = digitalocean_domain.glynbrook_com.id
+  type   = "NS"
+  name   = "@"
+  value  = "ns1.digitalocean.com."
+}
+
+resource "digitalocean_record" "ns2_glynbrook_com" {
+  domain = digitalocean_domain.glynbrook_com.id
+  type   = "NS"
+  name   = "@"
+  value  = "ns2.digitalocean.com."
+}
+
+resource "digitalocean_record" "ns3_glynbrook_com" {
+  domain = digitalocean_domain.glynbrook_com.id
+  type   = "NS"
+  name   = "@"
+  value  = "ns3.digitalocean.com."
+}
+
+##########
 # gras.so domain
+##########
 
 resource "digitalocean_record" "www_gras_so" {
   domain = digitalocean_domain.gras_so.id
