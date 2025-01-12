c92ff9844cfcc71591f5581ddd20fd1bdfedf482
Joey Grasso <me@joeygrasso.com>
2025-01-11 19:17:40 -0500

    Import and update grandslamgrasso.com domain and DNS

--- terraform/digitalocean/domains.tf
+++ terraform/digitalocean/domains.tf
@@ -2,6 +2,10 @@ resource "digitalocean_domain" "glynbrook_com" {
     name = "glynbrook.com"
 }
 
+resource "digitalocean_domain" "grandslamgrasso_com" {
+    name = "grandslamgrasso.com"
+}
+
 resource "digitalocean_domain" "gras_so" {
     name = "gras.so"
 }
