df63e9aadd71451ac29054d34137eb88b97a28c5
Joey Grasso <me@joeygrasso.com>
2025-07-29 14:44:35 -0400

    Remove gras.so domain and dns records

--- terraform/digitalocean/dns_records.tf
+++ terraform/digitalocean/dns_records.tf
@@ -228,45 +228,6 @@ resource "digitalocean_record" "ns3_grandslamgrasso_com" {
 }
 
 ##########
-# gras.so domain
-##########
-
-resource "digitalocean_record" "www_gras_so" {
-  domain = digitalocean_domain.gras_so.id
-  type   = "A"
-  name   = "www"
-  value  = digitalocean_reserved_ip.web-nyc1-cpunation-com-reserved-ip.ip_address
-}
-
-resource "digitalocean_record" "gras_so" {
-  domain = digitalocean_domain.gras_so.id
-  type   = "A"
-  name   = "@"
-  value  = digitalocean_reserved_ip.web-nyc1-cpunation-com-reserved-ip.ip_address
-}
-
-resource "digitalocean_record" "ns1_gras_so" {
-  domain = digitalocean_domain.gras_so.id
-  type   = "NS"
-  name   = "@"
-  value  = "ns1.digitalocean.com."
-}
-
-resource "digitalocean_record" "ns2_gras_so" {
-  domain = digitalocean_domain.gras_so.id
-  type   = "NS"
-  name   = "@"
-  value  = "ns2.digitalocean.com."
-}
-
-resource "digitalocean_record" "ns3_gras_so" {
-  domain = digitalocean_domain.gras_so.id
-  type   = "NS"
-  name   = "@"
-  value  = "ns3.digitalocean.com."
-}
-
-##########
 # joeygrasso.com
 ##########
 resource "digitalocean_record" "www_joeygrasso_com" {
